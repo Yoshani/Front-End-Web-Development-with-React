5cf697ddbf4cf718ad435f4ef93353430265afd2
Yoshani Ranaweera <yoshani.malinka@gmail.com>
2020-08-06 13:55:14 +0530

    redux actions

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -26,8 +26,8 @@ class CommentForm extends Component{
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -118,7 +118,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
 
     const feedback = comments.map((comment) => {
         return(
@@ -134,7 +134,7 @@ function RenderComments({comments}) {
             <div>
                 <h4><b>Comments</b></h4>
                 {feedback}
-                <CommentForm/>
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         );
     else
@@ -165,7 +165,10 @@ const  DishDetail = (props) => {
                 <RenderDish dish={props.dish} />
             </div>
             <div className="col-12 col-md-5 m-1">
-                <RenderComments comments={props.comments} />
+                <RenderComments comments={props.comments} 
+                addComment={props.addComment}
+                dishId={props.dish.id}
+                />
             </div>
         </div>
         </div>
